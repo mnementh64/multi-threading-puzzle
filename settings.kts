970d0c2ffadfab63529b95f0d3a6497b72f3ad93
scaillet <s.caillet@free.fr>
2023-03-26 18:48:22 +0200

    update CI

--- settings.kts
+++ settings.kts
@@ -1,26 +1,38 @@
-package _Self.buildTypes
-
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.templates.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.*
 
-object Build : BuildType({
-    name = "Build"
+object Project : Project({
+    id("Multi_threading_puzzle")
+    name = "Multi threading puzzle"
 
-    vcs {
-        root(HttpsGithubComMnementh64multiThreadingPuzzleGitRefsHeadsMaster)
-    }
-
-    steps {
-        maven {
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-            jdkHome = "%env.JDK_1_8%"
-        }
-    }
-
-    triggers {
+    // Configuration for the build runner
+    buildType({
+        name = "Build"
         vcs {
+            root(HttpsGithubComMnementh64multiThreadingPuzzleGitRefsHeadsMaster) // Define your VCS root here
         }
+        steps {
+            maven {
+                goals = "clean package" // Maven goals to execute
+                jdkHome = "%env.JDK_1_8%" // Path to Java 1.8 installation
+                jvmArgs = "-Xmx1024m" // JVM arguments
+                runnerArgs = "-Dmaven.test.failure.ignore=true" // Additional runner arguments
+            }
+        }
+    })
+})
+
+// Define VCS root
+object HttpsGithubComMnementh64multiThreadingPuzzleGitRefsHeadsMaster : GitVcsRoot({
+    id("HttpsGithubComMnementh64multiThreadingPuzzleGitRefsHeadsMaster")
+    name = "VCS root for this project"
+    url = "https://github.com/mnementh64/multi-threading-puzzle.git"
+    branch = "master"
+    authMethod = password {
+        userName = "mnementh64"
+        password = "!!Einstein64"
     }
 })
